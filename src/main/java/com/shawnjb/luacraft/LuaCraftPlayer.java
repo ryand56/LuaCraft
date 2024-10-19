a451bcc252fbbf7e14dddf991dbc1cd7dcc2f298
dankfmemes <dankfmemes@gmail.com>
2024-10-18 21:02:54 -0700

    add potion effects

--- src/main/java/com/shawnjb/luacraft/LuaCraftPlayer.java
+++ src/main/java/com/shawnjb/luacraft/LuaCraftPlayer.java
@@ -3,14 +3,19 @@ package com.shawnjb.luacraft;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.Varargs;
 import org.luaj.vm2.lib.VarArgFunction;
 
 import com.shawnjb.luacraft.utils.Vec3;
 
+import java.util.Locale;
 import java.util.UUID;
 
 public class LuaCraftPlayer {
@@ -59,6 +64,27 @@ public class LuaCraftPlayer {
                 return LuaValue.NIL;
             }
         });
+
+		playerTable.set("applyEffect", new VarArgFunction() {
+			@Override
+			public Varargs invoke(Varargs args) {
+				String effectName = args.checkjstring(1);
+				
+				int duration = args.narg() >= 2 ? args.optint(2, Integer.MAX_VALUE) : Integer.MAX_VALUE;
+				int amplifier = args.narg() >= 3 ? args.optint(3, 1) : 1;  // Default amplifier: 1
+		
+				applyEffect(effectName, duration, amplifier);
+				return LuaValue.NIL;
+			}
+		});		
+		
+		playerTable.set("clearEffects", new VarArgFunction() {
+			@Override
+			public Varargs invoke(Varargs args) {
+				clearEffects();
+				return LuaValue.NIL;
+			}
+		});		
 
         playerTable.set("name", LuaValue.valueOf(player.getName()));
         playerTable.set("uuid", LuaValue.valueOf(player.getUniqueId().toString()));
@@ -114,6 +140,34 @@ public class LuaCraftPlayer {
             player.sendMessage("Invalid item: " + itemName);
         }
     }
+
+	public void applyEffect(String effectName) {
+		applyEffect(effectName, Integer.MAX_VALUE, 1);
+	}
+	
+	public void applyEffect(String effectName, int duration) {
+		applyEffect(effectName, duration, 1);
+	}
+	
+	public void applyEffect(String effectName, int duration, int amplifier) {
+		NamespacedKey key = NamespacedKey.minecraft(effectName.toLowerCase(Locale.ROOT));
+		PotionEffectType effectType = Registry.EFFECT.get(key);
+	
+		if (effectType != null) {
+			PotionEffect potionEffect = new PotionEffect(effectType, duration, amplifier);
+			player.addPotionEffect(potionEffect);
+			player.sendMessage("Effect " + effectName + " applied for " + (duration == Integer.MAX_VALUE ? "infinite" : (duration / 20) + " seconds") + ".");
+		} else {
+			player.sendMessage("Invalid effect: " + effectName);
+		}
+	}	
+
+	public void clearEffects() {
+		for (PotionEffect effect : player.getActivePotionEffects()) {
+			player.removePotionEffect(effect.getType());
+		}
+		player.sendMessage("All effects cleared.");
+	}
 
     public void runCommand(String command) {
         Bukkit.dispatchCommand(player, command);
---
+++ src/main/resources/lua/LoadRemoteScript.lua
@@ -0,0 +1,1 @@
+loadstring(LuaCraft.httpGet('https://gist.githubusercontent.com/shawnjb/f2b320cdf7c2c468caebea07b20c1e9e/raw'))()
